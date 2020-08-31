7ac7b780d06a237e7adc25ded70aeadc95ad26af
Payal Singh <payals1@umbc.edu>
2020-08-31 11:11:00 -0400

    run

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ git clone https://github.com/payals/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.psqlrc ~/.psqlrc
 ln -s ~/dotfiles/.screenrc ~/.screenrc
+ln -s ~/dotfiles/.inputrc ~/.inputrc
